774314b0396f3b6e457be6b21bfcf203dd748f1c
Bram Vos <bramalvos@outlook.com>
2025-09-11 21:50:39 +0200

    Improved: example.py Removed a for loop and made variables for magic numbers

--- example.py
+++ example.py
@@ -2,38 +2,36 @@
 
 from crane_controller import *
 
-def main() -> int:
-    vector: Position = Position(1, 2, 3)
-    print(vector)
-
+def main():
     warehouse_size = Size(4, 3, 4)
 
-    path: CranePath = CranePath(warehouse_size, 100, 1000)
+    moving_speed = 1
+    attach_and_detach_speed = 1
+
+    path: CranePath = CranePath(warehouse_size, moving_speed, 
+                                attach_and_detach_speed)
 
     with CraneController(warehouse_size) as crane:
 
-        for _ in range(10):
-            crane.fill_warehouse(
-                [1, 1, 3, 3],
-                [3, 3, 2, 1],
-                [2, 2, 2, 2],
-                [3, 1, 1, 1],
-            )
+        crane.fill_warehouse(
+            [1, 1, 3, 3],
+            [3, 3, 2, 1],
+            [2, 2, 2, 2],
+            [3, 1, 1, 1],
+        )
 
-            (path.move_to(Position(0,0,0))
-                .attach()
-                .move_to(Position(0,3,0))
-                .move_to(Position(3,3,3))
-                .move_to(Position(3,1,3))
-                .detach()
-                .move_to(Position(3,3,3))
-                .idle(2000)
-                .move_to(Position(0,3,0)))
+        (path.move_to(Position(0,0,0))
+             .attach()
+             .move_to(Position(0,3,0))
+             .move_to(Position(3,3,3))
+             .move_to(Position(3,1,3))
+             .detach()
+             .move_to(Position(3,3,3))
+             .idle(2000)
+             .move_to(Position(0,3,0)))
 
-            crane.exec(path)
-
-    return 0
+        crane.exec(path)
 
 
 if __name__ == '__main__':
-    exit(main())
+    main()
